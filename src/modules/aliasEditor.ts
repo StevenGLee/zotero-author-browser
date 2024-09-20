648a3fdee347765b99a6a83da5430e518b18eb86
StevenGLee <lijt_001@sina.com>
2024-09-20 10:45:04 +0800

    chore(publish): release v0.1.3

---
+++ src/modules/aliasEditor.ts
@@ -0,0 +1,255 @@
+/// @ts-nocheck
+import { config } from "../../package.json";
+import { getLocaleID, getString } from "../utils/locale";
+import { isWindowAlive } from "../utils/window";
+import { CreatorDataRow } from "./authorBrowserAddon";
+
+export async function onDialog() {
+  refresh();
+
+  if (isWindowAlive(addon.data.manager.window)) {
+    addon.data.manager.window?.focus();
+    // refresh();
+  } else {
+    const windowArgs = {
+      _initPromise: Zotero.Promise.defer(),
+    };
+    const win = Zotero.getMainWindow().openDialog(
+      `chrome://${config.addonRef}/content/AliasEditor.xhtml`,
+      `${config.addonRef}-allAuthorWindow`,
+      `chrome,centerscreen,resizable,status,dialog=yes`,
+      windowArgs,
+    )!;
+    await windowArgs._initPromise.promise;
+    addon.data.manager.aliasEditor.window = win;
+    // updateData();
+    addon.data.manager.aliasEditor.nonAliastableHelper =
+      new ztoolkit.VirtualizedTable(win!)
+        .setContainerId("non-alias-table-container")
+        .setProp({
+          id: "author-list",
+          columns: [
+            {
+              dataKey: "firstName",
+              label: "firstName",
+              fixedWidth: false,
+            },
+            {
+              dataKey: "lastName",
+              label: "lastName",
+              fixedWidth: false,
+            },
+            {
+              dataKey: "itemCount",
+              label: "itemCount",
+              fixedWidth: false,
+            },
+          ].map((column) =>
+            Object.assign(column, {
+              label: getString(column.label),
+            }),
+          ),
+          showHeader: true,
+          multiSelect: false,
+          staticColumns: false,
+          disableFontSizeScaling: true,
+        })
+        .setProp("getRowCount", () => addon.data.manager.data.length)
+        .setProp("getRowData", (index) => ({
+          firstName: addon.data.manager.data[index].firstName,
+          lastName: addon.data.manager.data[index].lastName,
+          itemCount: String(addon.data.manager.data[index].itemCount),
+        }))
+        .setProp("onSelectionChange", (selection) => {
+          updateButtons();
+        })
+        .setProp("onActivate", (ev) => {
+          showAuthorByID(getSelectedNoteIds());
+          return true;
+        })
+        .setProp("onColumnSort", (columnIndex, ascending) => {
+          addon.data.manager.columnIndex = columnIndex;
+          addon.data.manager.columnAscending = ascending > 0;
+          quickSort();
+        })
+        .render();
+    addon.data.manager.aliasEditor.aliastableHelper =
+      new ztoolkit.VirtualizedTable(win!)
+        .setContainerId("alias-table-container")
+        .setProp({
+          id: "author-list",
+          columns: [
+            {
+              dataKey: "firstName",
+              label: "firstName",
+              fixedWidth: false,
+            },
+            {
+              dataKey: "lastName",
+              label: "lastName",
+              fixedWidth: false,
+            },
+            {
+              dataKey: "itemCount",
+              label: "itemCount",
+              fixedWidth: false,
+            },
+            {
+              dataKey: "aliasFullNames",
+              label: "aliasFullNames",
+              fixedWidth: false,
+            },
+          ].map((column) =>
+            Object.assign(column, {
+              label: getString(column.label),
+            }),
+          ),
+          showHeader: true,
+          multiSelect: false,
+          staticColumns: false,
+          disableFontSizeScaling: true,
+        })
+        .setProp("getRowCount", () => addon.data.manager.data.length)
+        .setProp("getRowData", (index) => ({
+          firstName: addon.data.manager.data[index].firstName,
+          lastName: addon.data.manager.data[index].lastName,
+          itemCount: String(addon.data.manager.data[index].itemCount),
+        }))
+        .setProp("onSelectionChange", (selection) => {
+          updateButtons();
+        })
+        .setProp("onActivate", (ev) => {
+          showAuthorByID(getSelectedNoteIds());
+          return true;
+        })
+        .setProp("onColumnSort", (columnIndex, ascending) => {
+          addon.data.manager.columnIndex = columnIndex;
+          addon.data.manager.columnAscending = ascending > 0;
+          quickSort();
+        })
+        .render();
+    const refreshButton = win.document.querySelector(
+      "#refresh",
+    ) as HTMLButtonElement;
+    const renameButton = win.document.querySelector(
+      "#rename",
+    ) as HTMLButtonElement;
+    const aliasButton = win.document.querySelector(
+      "#alias",
+    ) as HTMLButtonElement;
+    const swapButton = win.document.querySelector("#swap") as HTMLButtonElement;
+    const fixCapssButton = win.document.querySelector(
+      "#fix-caps",
+    ) as HTMLButtonElement;
+    const showItemsButton = win.document.querySelector(
+      "#show-item",
+    ) as HTMLButtonElement;
+    refreshButton.addEventListener("click", (ev) => {
+      refresh();
+    });
+    renameButton.disabled = true;
+    renameButton.addEventListener("click", async (ev) => {
+      refresh();
+    });
+    aliasButton.disabled = true;
+    aliasButton.addEventListener("click", (ev) => {
+      refresh();
+    });
+
+    swapButton.addEventListener("click", () => {
+      swapNames();
+      refresh();
+    });
+    fixCapssButton.addEventListener("click", () => {
+      capitalizeCreatorName();
+    });
+    showItemsButton.addEventListener("click", () => {
+      showAuthorByID(getSelectedNoteIds());
+    });
+  }
+}
+
+const sortDataKeys = ["firstName", "lastName", "itemCount"];
+
+async function quickSort() {
+  const sortKey = sortDataKeys[addon.data.manager.columnIndex];
+  addon.data.manager.data.sort((a, b) => {
+    if (!a || !b) {
+      return 0;
+    }
+    const valueA = String(a[sortKey] || "");
+    const valueB = String(b[sortKey] || "");
+    return addon.data.manager.columnAscending
+      ? valueA.localeCompare(valueB)
+      : valueB.localeCompare(valueA);
+  });
+
+  await updateTable();
+  updateButtons();
+}
+
+async function updateData() {
+  const sortKey = sortDataKeys[addon.data.manager.columnIndex];
+  addon.data.manager.data = (await getAllCreators(sortKey, addon.data.manager.columnAscending == false));
+}
+function updateButtons() {
+  const win = addon.data.manager.window;
+  if (!win) {
+    return;
+  }
+  const fixCapssButton = win.document.querySelector(
+    "#fix-caps",
+  ) as HTMLButtonElement;
+  if (canCapitalizeCreatorName(getSelectedNoteIds())) {
+    fixCapssButton.disabled = false;
+  } else {
+    fixCapssButton.disabled = true;
+  }
+}
+async function updateTable() {
+  return new Promise<void>((resolve) => {
+    addon.data.manager.tableHelper?.render(undefined, (_) => {
+      resolve();
+    });
+  });
+}
+async function refresh() {
+  await updateData();
+  await updateTable();
+  updateButtons();
+}
+function getSelectedNoteIds() {
+  let id: number = -1;
+  for (const idx of addon.data.manager.tableHelper?.treeInstance.selection.selected?.keys() ||
+    []) {
+    id = addon.data.manager.data[idx].creatorID;
+  }
+  return id;
+}
+async function swapNames() {
+  const creatorID = getSelectedNoteIds();
+  const fields = Zotero.Creators.get(creatorID);
+  const lastName = fields.lastName;
+  const firstName = fields.firstName;
+  fields.lastName = firstName;
+  fields.firstName = lastName;
+  Zotero.Creators.updateCreator(creatorID, fields);
+}
+
+function canCapitalizeCreatorName(creatorID: number) {
+  const fields = Zotero.Creators.get(creatorID);
+  return (
+    (fields.firstName &&
+      Zotero.Utilities.capitalizeName(fields.firstName) != fields.firstName) ||
+    (fields.lastName &&
+      Zotero.Utilities.capitalizeName(fields.lastName) != fields.lastName)
+  );
+}
+
+async function capitalizeCreatorName() {
+  const creatorID = getSelectedNoteIds();
+  const fields = Zotero.Creators.get(creatorID);
+  fields.lastName = Zotero.Utilities.capitalizeName(fields.lastName);
+  fields.firstName = Zotero.Utilities.capitalizeName(fields.firstName);
+  Zotero.Creators.updateCreator(creatorID, fields);
+}
